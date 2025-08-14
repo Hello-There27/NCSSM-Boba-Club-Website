a1561a3833a812d3c194e3683135b23d95708221
Hello-There27 <muhilank27@gmail.com>
2025-08-14 14:16:26 -0400

    Redid supabase connection

--- src/supabase.js
+++ src/supabase.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
 // Replace these with your actual Supabase project credentials
-const supabaseUrl = 'https://urubjyrazejwqckfozwb.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVydWJqeXJhemVqd3Fja2ZvendiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM4NDkxMjcsImV4cCI6MjA2OTQyNTEyN30.i2v14izxR-qwHyNYUvs3ZEl91-Sp_hPMePKvdXaq9uY'
+const supabaseUrl = 'https://ykwpsojpnfqwqtpxtccv.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlrd3Bzb2pwbmZxd3F0cHh0Y2N2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM4NDkxMjEsImV4cCI6MjA2OTQyNTEyMX0.fRAUneAPz1a7krlsU7le7-g4Ub0pogfyqoeNOcGw1RE'
 
 
 if (!supabaseUrl || !supabaseAnonKey) {
